c5b5d4a3e4831bae6a5b2941c2fc6e589c7ac44b
Sean Landsman <sean@thelandsmans.com>
2016-11-09 11:42:18 +0000

    AOT work

--- copy-dist-files.js
+++ copy-dist-files.js
@@ -2,11 +2,12 @@ var fs = require('fs');
 var resources = [
     'node_modules/core-js/client/shim.min.js',
     'node_modules/zone.js/dist/zone.min.js',
+    'node_modules/systemjs/dist/system.src.js',
     'node_modules/ag-grid/dist/styles/ag-grid.css',
     'node_modules/ag-grid/dist/styles/theme-fresh.css'
 ];
 resources.map(function (f) {
     var path = f.split('/');
-    var t = 'lib/' + path[path.length - 1];
+    var t = 'aot/' + path[path.length - 1];
     fs.createReadStream(f).pipe(fs.createWriteStream(t));
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 var gulp = require('gulp');
 var SystemBuilder = require('systemjs-builder');
 
-gulp.task('demo-bundle', function () {
+gulp.task('aot-bundle', function () {
     var builder = new SystemBuilder();
 
     builder.loadConfig('./systemjs.config.js')
         .then(function () {
-            // return builder.buildStatic('lib/app/boot.js', './bundle.js', {
-            return builder.buildStatic('app/boot.js', './bundle.js', {
+            return builder.buildStatic('aot/app/boot-aot.js', './aot/dist/bundle.js', {
                 encodeNames: false,
                 mangle: false,
-                rollup: true
+                minify: true,
+                rollup: true,
+                sourceMaps: true
             });
         })
 });
