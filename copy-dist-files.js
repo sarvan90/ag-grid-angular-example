1214a64fe3a467f139dd34672ef1a8c71ca9221d
Sean Landsman <sean@thelandsmans.com>
2016-11-29 16:42:21 +0000

    Tidy dependencies

--- copy-dist-files.js
+++ copy-dist-files.js
@@ -4,7 +4,8 @@ var resources = [
     'node_modules/zone.js/dist/zone.min.js',
     'node_modules/systemjs/dist/system.src.js',
     'node_modules/ag-grid/dist/styles/ag-grid.css',
-    'node_modules/ag-grid/dist/styles/theme-fresh.css'
+    'node_modules/ag-grid/dist/styles/theme-fresh.css',
+    'node_modules/bootstrap/dist/css/bootstrap.css'
 ];
 resources.map(function (f) {
     var path = f.split('/');
